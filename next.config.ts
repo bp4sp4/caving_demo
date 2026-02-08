dba9b2bba133bf674f3864b4c6cf2d852d84b50a
박상훈 <bp4sp4@naver.com>
2026-02-08 14:55:33 +0900

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // GitHub Pages에 정적 export로 배포하기 위한 설정
-  output: "export",
-  // 리포지토리 이름으로 basePath/assetPrefix를 설정합니다. 필요 시 변경하세요.
-  basePath: "/caving_demo",
-  assetPrefix: "/caving_demo",
+
 };
 
 export default nextConfig;
